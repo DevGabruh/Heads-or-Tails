b39d7525fef573b582aec6574576394bb396d1ad
DevGabruh <>
2021-05-06 04:58:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module first. 🎲
+import random
+
+random_n = random.randint(0, 1)
+
+if random_n == 0:
+  print("Heads")
+else:
+  print("Tails")
